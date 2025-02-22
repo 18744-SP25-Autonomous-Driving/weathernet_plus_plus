2b134440cba6bf6bce608e0d86c9ac94e0f4f3cd
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2025-02-21 19:27:07 -0500

    updated loss criterion for weathernet imported old training script and started modifying to fit our scenario

--- weathernet_base/weathernet.py
+++ weathernet_base/weathernet.py
@@ -1,4 +1,5 @@
 '''WeatherNet: Implementation of the WeatherNet model.'''
+from typing import List, Tuple, TypedDict
 import torch
 import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
@@ -6,11 +7,30 @@ from torchvision.models import resnet50, ResNet50_Weights
 
 class WeatherNet(nn.Module):
     '''WeatherNet: Implementation of the WeatherNet model.'''
+    class LossCriterion():
+        '''LossCriterion: TypedDict for loss criterion object.'''
+        def __init__(self, loss_dict: dict) -> None:
+            self.night_loss: torch.nn.BCEWithLogitsLoss = loss_dict['night_loss']
+            self.glare_loss: torch.nn.BCEWithLogitsLoss = loss_dict['glare_loss']
+            self.fog_loss: torch.nn.BCEWithLogitsLoss = loss_dict['fog_loss']
+            self.precip_loss: torch.nn.CrossEntropyLoss = loss_dict['precip_loss']
+            self.total_loss: int = loss_dict['total_loss']
+
+        def backward(self):
+            '''backward pass of loss criterion'''
+            self.night_loss().backward()
+            self.glare_loss().backward()
+            self.fog_loss().backward()
+            self.precip_loss().backward()
+
+
+
+
     def __init__(self) -> None:
         super(WeatherNet, self).__init__()
 
         # night-net: resnet50, replaced linear layer at end to be one output, followed by sigmoid.
-        # Time of day is a single class, so we use a single output with sigmoid activation 
+        # Time of day is a single class, so we use a single output with sigmoid activation
         # to predict the probability of night.
         self.night_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
         self.night_net.fc = nn.Linear(self.night_net.fc.in_features, 1)
@@ -21,7 +41,7 @@ class WeatherNet(nn.Module):
         self.glare_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
         self.glare_net.fc = nn.Linear(self.glare_net.fc.in_features, 1)
 
-        # precipitation-net: resnet50, replaced linear layer at end to be TWO output, followed
+        # precipitation-net: resnet50, replaced linear layer at end to be THREE output, followed
         # by softmax.Precipitation is three classes (rain, snow, clear), so we use three outputs
         # with softmax activation to predict the probability of each class. First element is rain,
         # second is snow.
@@ -39,10 +59,11 @@ class WeatherNet(nn.Module):
         # combines cross entropy with a sigmoid activation function in a single class,
         # making it more numerically stable.
         # We use this for night, glare, and fog.
-        self.binary_criterion = nn.BCEWithLogitsLoss()
-
-        # Softmax is internally computed. We use this for precipitation.
-        self.multi_criterion = nn.CrossEntropyLoss()
+        # Precipitation is multiclass, so we use CrossEntropyLoss (softmax).
+        self.night_loss = nn.BCEWithLogitsLoss()
+        self.glare_loss = nn.BCEWithLogitsLoss()
+        self.fog_loss = nn.BCEWithLogitsLoss()
+        self.precipitation_loss = nn.CrossEntropyLoss()
 
 
     def forward(self,x):
@@ -61,17 +82,25 @@ class WeatherNet(nn.Module):
 
         return torch.cat([night, glare, precipitation, fog], dim=1)
 
-    def criterion(self, predictions, labels):
-        '''compute loss of model outputs'''
+    def criterion(self, predictions, labels)->LossCriterion:
+        '''
+        compute loss of model outputs.
+        Returns night_loss, glare_loss, fog_loss, precipitation_loss objects
+        '''
         night, glare, precipitation, fog = predictions.split(1, 1, 3, 1, dim=1)
 
-        night_loss  = self.binary_criterion(night, labels[:, 0].unsqueeze(1))
-        glare_loss  = self.binary_criterion(glare, labels[:, 1].unsqueeze(1))
-        fog_loss    = self.binary_criterion(fog, labels[:, 2].unsqueeze(1))
-        precipitation_loss = self.multi_criterion(precipitation, labels[:, 3].long())
+        night_loss_val  = self.night_loss(night, labels[:, 0].unsqueeze(1))
+        glare_loss_val  = self.glare_loss(glare, labels[:, 1].unsqueeze(1))
+        fog_loss_val    = self.fog_loss(fog, labels[:, 2].unsqueeze(1))
+        precipitation_loss_val = self.precipitation_loss(precipitation, labels[:, 3].long())
 
-        total_loss = night_loss + glare_loss + fog_loss + precipitation_loss
-        return total_loss, night_loss, glare_loss, fog_loss, precipitation_loss
+        return WeatherNet.LossCriterion({
+            'night_loss': self.night_loss,
+            'glare_loss': self.glare_loss,
+            'fog_loss': self.fog_loss,
+            'precip_loss': self.precipitation_loss,
+            'total_loss': night_loss_val + glare_loss_val + fog_loss_val + precipitation_loss_val
+        })
 
     def compute_metrics(self, predictions, labels):
         '''compute metrics of model outputs'''
