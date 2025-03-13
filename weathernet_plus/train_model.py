746d998d8ff896d6aae59f2907add4419a6122ff
Josiah Miggiani <jmiggian@andrew.cmu.edu>
2025-03-13 16:16:00 -0400

    Gets WeatherNetPlus running. Reorders much of the code for consistency

--- weathernet_plus/train_model.py
+++ weathernet_plus/train_model.py
@@ -3,34 +3,34 @@ Main Training Code for models
 """
 
 import os
-import random
 import time
 import argparse
 from typing import Tuple
 
 import torch
-import torch.nn as nn
 import torch.utils.data.dataloader
 from torch.utils.data import DataLoader
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
-import numpy as np
 
 from set_seed import set_random_seed
 from weathernetplusplus import WeatherNetPlusPlus
+
+from BDD100K_plus import BDD100K_plus
+import pdb
 
 # Argument parser
 parser: argparse.ArgumentParser = argparse.ArgumentParser(
     description="18744 Autonomous Driving Project - Model Trainer"
 )
 
-# Define the mini-batch size, here the size is 128 images per batch
+# Define the mini-batch size, here the size is 32 images per batch
 parser.add_argument(
-    "--batch_size", type=int, default=128, help="Number of samples per mini-batch"
+    "--batch_size", type=int, default=32, help="Number of samples per mini-batch"
 )
 
 # Define the number of epochs for training
-parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train")
+parser.add_argument("--epochs", type=int, default=20, help="Number of epochs to train")
 
 # Define the random seed
 parser.add_argument(
@@ -53,7 +53,41 @@ saved_state: str = args.saved_state
 # Set Random Seed (reproducibility)
 set_random_seed(random_seed)
 
+### BDD100K_plus DATASET (IMAGES & LABELS) ###
+train_dataset = BDD100K_plus(
+    root="data",
+    train=True,
+    transform=transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(
+                # experimentally determined
+                mean=(0.2843, 0.3026, 0.2996),
+                std=(0.1918, 0.1945, 0.1989)
+            ),
+            transforms.Resize((224, 224))
+        ]
+    ),
+    download=False,
+)
 
+test_dataset = BDD100K_plus(
+    root="data",
+    train=False,
+    transform=transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=(0.2843, 0.3026, 0.2996),
+                std=(0.1918, 0.1945, 0.1989)
+            ),
+            transforms.Resize((224, 224))
+        ]
+    ),
+    download=False,
+)
+
+'''
 # CIFAR10 Dataset (Images and Labels) (for TESTING ONLY)
 train_dataset: dsets.CIFAR10 = dsets.CIFAR10(
     root="data",
@@ -82,6 +116,7 @@ test_dataset: dsets.CIFAR10 = dsets.CIFAR10(
         ]
     ),
 )
+'''
 
 # Load the datasets into torch dataloaders
 train_loader: DataLoader[Tuple[torch.Tensor, int]] = DataLoader(
@@ -117,45 +152,25 @@ print(model.parameters())
 optimizer = torch.optim.Adam(model.parameters())
 
 
-# Label mapping function ONLY FOR CIFAR TESTING, DELETE ONCE BDD100K IS IMPORTED
-def map_labels(cifar_labels):
+def map_labels(labels):
     """
-    Convert CIFAR-10 labels to our custom labels for night-net, glare-net, and weather-net.
+    Separates labels for all 7 categories: fog, glare, road, traffic, weather, scene, timeofday 
+    for WeatherNetPlus
     """
-    _night_labels = torch.where(
-        (cifar_labels == 0)
-        | (cifar_labels == 1)
-        | (cifar_labels == 2)
-        | (cifar_labels == 3),
-        cifar_labels,
-        torch.tensor(3),
-    )  # Map 0,1,2, to day/night, daytime, night, map all others to undefined
 
-    _glare_labels = (
-        cifar_labels == 1
-    ).float()  # Binary: 1 for automobile, 0 for others
+    # TODO: Create enums for the pipeline categories, e.g. FOG = 0
+    # technically, this could be reduced. But this makes it more readable.
+    _fog_labels     = labels[:, 0]
+    _glare_labels   = labels[:, 1]
+    _road_labels    = labels[:, 2]
+    _traffic_labels = labels[:, 3]
+    _weather_labels = labels[:, 4]
+    _scene_labels   = labels[:, 5]
+    _night_labels   = labels[:, 6]
 
-    _weather_labels = torch.full_like(cifar_labels, 4)  # Default to "undefined"
-    for i, label in enumerate([2, 3, 4, 5, 6]):  # bird, cat, deer, dog, frog
-        _weather_labels[cifar_labels == label] = i  # Map to 0-4
-
-    _scene_labels = torch.full_like(cifar_labels, 6)  # Default to "undefined"
-    for i, label in enumerate([0, 1, 2, 3, 4, 5]):  # six classes
-        _scene_labels[cifar_labels == label] = i
-
-    _road_labels = (cifar_labels == 7).float()  # Binary: 1 for automobile, 0 for others
-    _traffic_labels = (
-        cifar_labels == 8
-    ).float()  # Binary: 1 for automobile, 0 for others
-
-    return (
-        _night_labels.to(device),
-        _glare_labels.to(device),
-        _weather_labels.to(device),
-        _scene_labels.to(device),
-        _road_labels.to(device),
-        _traffic_labels.to(device),
-    )
+    return (_fog_labels.to(device), _glare_labels.to(device), _road_labels.to(device), 
+            _traffic_labels.to(device), _weather_labels.to(device), _scene_labels.to(device), 
+            _night_labels.to(device))
 
 
 # Training loop
@@ -177,31 +192,42 @@ for epoch in range(args.epochs):
     for batch_idx, (images, labels) in enumerate(train_loader):
         images, labels = images.to(device), labels.to(device)
         (
-            night_labels,
+            fog_labels,
             glare_labels,
-            weather_labels,
-            scene_labels,
             road_labels,
             traffic_labels,
+            weather_labels,
+            scene_labels,
+            night_labels,
         ) = map_labels(labels)
 
         optimizer.zero_grad()
 
         # Forward pass
-        (night_pred, glare_pred, weather_pred, scene_pred, road_pred, traffic_pred) = (
+        (fog_pred, glare_pred, road_pred, traffic_pred, weather_pred, scene_pred, night_pred) = (
             model(images)
         )
 
+        # breakpoint()
         # Compute loss
         loss = model.compute_loss(
-            (night_pred, glare_pred, weather_pred, scene_pred, road_pred, traffic_pred),
             (
-                night_labels,
+                fog_pred, 
+                glare_pred, 
+                road_pred, 
+                traffic_pred, 
+                weather_pred, 
+                scene_pred, 
+                night_pred
+            ),
+            (
+                fog_labels,
                 glare_labels,
-                weather_labels,
-                scene_labels,
                 road_labels,
                 traffic_labels,
+                weather_labels,
+                scene_labels,
+                night_labels,
             ),
         )
         training_loss += loss.item()
@@ -211,23 +237,28 @@ for epoch in range(args.epochs):
         optimizer.step()
 
         # Calculate accuracy
-        _, night_predicted = night_pred.max(1)
+        _, road_predicted = road_pred.max(1)
+        _, traffic_predicted = traffic_pred.max(1)
         _, weather_predicted = weather_pred.max(1)
         _, scene_predicted = scene_pred.max(1)
+        _, night_predicted = night_pred.max(1)
         training_total += (
-            night_labels.size(0) + 
+            fog_labels.size(0) +
             glare_labels.size(0) +
-            weather_labels.size(0) + 
-            scene_labels.size(0) +
             road_labels.size(0) +
-            traffic_labels.size(0)
+            traffic_labels.size(0) +
+            weather_labels.size(0) +
+            scene_labels.size(0) +
+            night_labels.size(0)
         )
-        training_correct += (night_predicted == night_labels).sum().item()
+        training_correct += (fog_pred.squeeze() > 0.5).eq(fog_labels).sum().item()
+        training_correct += (glare_pred.squeeze() > 0.5).eq(glare_labels).sum().item()
+
+        training_correct += (road_predicted == road_labels).sum().item()
+        training_correct += (traffic_predicted == traffic_labels).sum().item()
         training_correct += (weather_predicted == weather_labels).sum().item()
         training_correct += (scene_predicted == scene_labels).sum().item()
-        training_correct += (glare_pred.squeeze() > 0.5).eq(glare_labels).sum().item()
-        training_correct += (road_pred.squeeze() > 0.5).eq(road_labels).sum().item()
-        training_correct += (traffic_pred.squeeze() > 0.5).eq(traffic_labels).sum().item()
+        training_correct += (night_predicted == night_labels).sum().item()
 
         if (batch_idx + 1) % 5 == 0:
             print(
@@ -251,58 +282,66 @@ for epoch in range(args.epochs):
         for batch_idx, (images, labels) in enumerate(test_loader):
             images, labels = images.to(device), labels.to(device)
             (
-                night_labels,
+                fog_labels,
                 glare_labels,
-                weather_labels,
-                scene_labels,
                 road_labels,
                 traffic_labels,
+                weather_labels,
+                scene_labels,
+                night_labels,
             ) = map_labels(labels)
 
-             # Forward pass
-            (night_pred, glare_pred, weather_pred, scene_pred, road_pred, traffic_pred) = (
+            # Forward pass
+            (fog_pred, glare_pred, road_pred, traffic_pred, weather_pred, scene_pred, night_pred) = (
                 model(images)
             )
 
             # Compute loss
             loss = model.compute_loss(
                 (
-                    night_pred, 
+                    fog_pred, 
                     glare_pred, 
+                    road_pred, 
+                    traffic_pred, 
                     weather_pred, 
                     scene_pred, 
-                    road_pred, 
-                    traffic_pred
+                    night_pred
                 ),
                 (
-                    night_labels,
+                    fog_labels,
                     glare_labels,
-                    weather_labels,
-                    scene_labels,
                     road_labels,
                     traffic_labels,
+                    weather_labels,
+                    scene_labels,
+                    night_labels,
                 ),
             )
             test_loss += loss.item()
 
             # Calculate accuracy
-            _, night_predicted = night_pred.max(1)
+            _, road_predicted = road_pred.max(1)
+            _, traffic_predicted = traffic_pred.max(1)
             _, weather_predicted = weather_pred.max(1)
             _, scene_predicted = scene_pred.max(1)
+            _, night_predicted = night_pred.max(1)
             testing_total += (
-                night_labels.size(0) +
+                fog_labels.size(0) +
                 glare_labels.size(0) +
+                road_labels.size(0) +
+                traffic_labels.size(0) +
                 weather_labels.size(0) +
                 scene_labels.size(0) +
-                road_labels.size(0) +
-                traffic_labels.size(0)
+                night_labels.size(0)
             )
-            testing_correct += (night_predicted == night_labels).sum().item()
+            testing_correct += (fog_pred.squeeze() > 0.5).eq(fog_labels).sum().item()
+            testing_correct += (glare_pred.squeeze() > 0.5).eq(glare_labels).sum().item()
+
+            testing_correct += (road_predicted == road_labels).sum().item()
+            testing_correct += (traffic_predicted == traffic_labels).sum().item()
             testing_correct += (weather_predicted == weather_labels).sum().item()
             testing_correct += (scene_predicted == scene_labels).sum().item()
-            testing_correct += (glare_pred.squeeze() > 0.5).eq(glare_labels).sum().item()
-            testing_correct += (road_pred.squeeze() > 0.5).eq(road_labels).sum().item()
-            testing_correct += (traffic_pred.squeeze() > 0.5).eq(traffic_labels).sum().item()
+            testing_correct += (night_predicted == night_labels).sum().item()
     print(
         "Test loss:",
         f"{test_loss / (batch_idx + 1):.4f}",
--- weathernet_plus/weathernetplusplus.py
+++ weathernet_plus/weathernetplusplus.py
@@ -4,7 +4,7 @@ augmented to to include scene detection, road detection, and traffic detection.
 to conform to BDD100K dataset labels
 """
 
-from typing import List, Tuple, TypedDict
+from typing import List, Tuple, TypedDict, NamedTuple
 import torch
 import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
@@ -13,28 +13,27 @@ from torchvision.models import resnet50, ResNet50_Weights
 class WeatherNetPlusPlus(nn.Module):
     """WeatherNet: Implementation of the WeatherNet model."""
 
+    class WeatherNetPlusPlusOutput(NamedTuple):
+        '''
+        Structure of the output
+        of the forward pass of the model.
+        '''
+        fog_pred: torch.Tensor
+        glare_pred: torch.Tensor
+        road_pred: torch.Tensor
+        traffic_pred: torch.Tensor
+        weather_pred: torch.Tensor
+        scene_pred: torch.Tensor
+        night_pred: torch.Tensor
+
     def __init__(self) -> None:
         super(WeatherNetPlusPlus, self).__init__()
 
-        # night-net: resnet50, replaced linear layer at end to be FOUR output, followed by softmax.
-        # Time of day is 4 classes (dawn/dusk, daytime, night, undefined), so we use a four output
-        # neurons with softmax activation to predict the probability of each one.
-        self.night_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-        self.night_net.fc = nn.Linear(self.night_net.fc.in_features, 4)
-
-        # weather-net: resnet50, replaced linear layer at end to be FIVE output,
-        # followed by softmax.
-        # Weather is five classes (clear, rain, snow, partly cloudy, undefined),
-        # so we use five outputs with
-        # softmax activation to predict the probability of each class.
-        self.weather_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-        self.weather_net.fc = nn.Linear(self.weather_net.fc.in_features, 5)
-
-        # scene-net: resnet50, replaced linear layer at end to be SIX output, followed by softmax.
-        # Scene is six classes ('city street', 'highway', 'residential', 'undefined', 'parking lot', 'tunnel'),
-        # so we use six outputs with softmax activation to predict the probability of each class.
-        self.scene_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-        self.scene_net.fc = nn.Linear(self.scene_net.fc.in_features, 6)
+        # fog-net: resnet50, replaced linear layer at end to be ONE output, followed by sigmoid.
+        # fog is a single class, so we use a single output with sigmoid activation to predict
+        # the probability of fog.
+        self.fog_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.fog_net.fc = nn.Linear(self.fog_net.fc.in_features, 1)
 
         # glare-net: resnet50, replaced linear layer at end to be ONE output, followed by sigmoid.
         # Glare is a single class, so we use a single output with sigmoid activation to predict
@@ -42,17 +41,35 @@ class WeatherNetPlusPlus(nn.Module):
         self.glare_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
         self.glare_net.fc = nn.Linear(self.glare_net.fc.in_features, 1)
 
-        # road-net: resnet50, replaced linear layer at end to be ONE output, followed by softmax.
-        # Road is single class, so we use one output with sigmoid activation to predict
-        # the probability of it being road
+        # road-net: resnet50, replaced linear layer at end to be THREE outputs, followed by softmax.
+        # road is THREE classes (dry road, wet road, snowy road)
+        # so we use three outputs with softmax activation to predict the probability of each class.
         self.road_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-        self.road_net.fc = nn.Linear(self.road_net.fc.in_features, 1)
+        self.road_net.fc = nn.Linear(self.road_net.fc.in_features, 3)
 
-        # traffic-net: resnet50, replaced linear layer at end to be ONE output, followed by softmax.
-        # Traffic is single class, so we use one output with sigmoid activation to predict
-        # the probability of it being traffic
+        # traffic-net: resnet50, replaced linear layer at end to be 3 outputs, followed by softmax.
+        # traffic is THREE classes (no traffic, low/moderate traffic, high traffic)
+        # so we use three outputs with softmax activation to predict the probability of each class.
         self.traffic_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-        self.traffic_net.fc = nn.Linear(self.traffic_net.fc.in_features, 1)
+        self.traffic_net.fc = nn.Linear(self.traffic_net.fc.in_features, 3)
+
+        # weather-net: resnet50, replaced linear layer at end to be SIX outputs, followed by softmax.
+        # weather is SIX classes (clear, partly cloudy, overcast, rainy, snowy, undefined)
+        # so we use six outputs with softmax activation to predict the probability of each class.
+        self.weather_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.weather_net.fc = nn.Linear(self.weather_net.fc.in_features, 6)
+
+        # scene-net: resnet50, replaced linear layer at end to be FOUR outputs, followed by softmax.
+        # scene is FOUR classes (residential, highway, city street, undefined),
+        # so we use FOUR outputs with softmax activation to predict the probability of each class.
+        self.scene_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.scene_net.fc = nn.Linear(self.scene_net.fc.in_features, 4)
+
+        # night-net: resnet50, replaced linear layer at end to be FOUR output, followed by softmax.
+        # timeofday is FOUR classes (dawn/dusk, daytime, night, undefined), so we use four output
+        # neurons with softmax activation to predict the probability of each class.
+        self.night_net = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.night_net.fc = nn.Linear(self.night_net.fc.in_features, 4)
 
         # Define loss functions
         # Binary Cross Entropy with Logits Loss for binary classification
@@ -60,94 +77,108 @@ class WeatherNetPlusPlus(nn.Module):
         # making it more numerically stable.
         # For multiclass, we use CrossEntropyLoss (softmax).
 
-        self.night_loss = nn.CrossEntropyLoss()
+        self.fog_loss = nn.BCEWithLogitsLoss()
+        self.glare_loss = nn.BCEWithLogitsLoss()
+
+        self.road_loss = nn.CrossEntropyLoss()
+        self.traffic_loss = nn.CrossEntropyLoss()
         self.weather_loss = nn.CrossEntropyLoss()
         self.scene_loss = nn.CrossEntropyLoss()
+        self.night_loss = nn.CrossEntropyLoss()
 
-        self.glare_loss = nn.BCEWithLogitsLoss()
-        self.road_loss = nn.BCEWithLogitsLoss()
-        self.traffic_loss = nn.BCEWithLogitsLoss()
 
     def forward(self, x):
         """
         forward pass
         gets called by model()
-        returns a Tuple (night_pred, glare_pred, weather_pred)
+        returns a Tuple (fog_pred, glare_pred, road_pred, traffic_pred, weather_pred, scene_pred, night_pred)
         """
-        # night-net prediction, 4 classes
-        night = self.night_net(x)
+        # fog-net prediction, 1 class
+        fog = self.fog_net(x)
 
         # glare-net prediction, 1 class
         glare = self.glare_net(x)
 
-        # weather-net prediction, 5 classes
-        weather = self.weather_net(x)
-
-        # scene-net prediction, 6 classes
-        scene = self.scene_net(x)
-
-        # road-net prediction, 1 class
+        # road-net prediction, 3 classes
         road = self.road_net(x)
 
-        # traffic-net prediction, 1 class
+        # traffic-net prediction, 3 class
         traffic = self.traffic_net(x)
 
-        return night, glare, weather, scene, road, traffic
+        # weather-net prediction, 6 classes
+        weather = self.weather_net(x)
+
+        # scene-net prediction, 4 classes
+        scene = self.scene_net(x)
+
+        # night-net prediction, 4 classes
+        night = self.night_net(x)
+
+        return WeatherNetPlusPlus.WeatherNetPlusPlusOutput(
+            fog, glare, road, traffic, weather, scene, night
+        )
 
     def compute_loss(self, predictions, targets):
         """
         Compute total loss.
         Args:
-            predictions: Tuple (night_pred, glare_pred, weather_pred, scene_pred, road_pred, traffic_pred)
-            targets: Tuple (night_target, glare_target, weather_target, scene_target, road_target, traffic_target)
+            predictions: Tuple (fog_pred, glare_pred, road_pred, traffic_pred, weather_pred, scene_pred, night_pred)
+            targets: Tuple (fog_target, glare_target, road_target, traffic_target, weather_target, scene_target, night_target)
         Returns:
             Total loss (scalar)
         """
         (
-            night_pred,
+            fog_pred,
             glare_pred,
+            road_pred,
+            traffic_pred,
             weather_pred,
             scene_pred,
-            road_pred,
-            traffic_pred
+            night_pred,
         ) = predictions
-        
+
         (
-            night_target,
+            fog_target,
             glare_target,
-            weather_target,
-            scene_target,
             road_target,
             traffic_target,
+            weather_target,
+            scene_target,
+            night_target,
         ) = targets
+
 
         # Compute individual losses
 
-        # CrossEntropyLoss expects (batch, 4) logits and (batch,) labels
-        loss_night = self.night_loss(night_pred, night_target)
+        # BCEWithLogitsLoss expects (batch,) logits and (batch,) labels
+        loss_fog = self.fog_loss(fog_pred.squeeze(), fog_target.float())
 
         # BCEWithLogitsLoss expects (batch,) logits and (batch,) labels
         loss_glare = self.glare_loss(glare_pred.squeeze(), glare_target.float())
 
-        # CrossEntropyLoss expects (batch, 5) logits and (batch,) labels
-        loss_weather = self.weather_loss(weather_pred, weather_target)
+        # CrossEntropyLoss expects (batch, 3) logits and (batch,) labels
+        loss_road = self.road_loss(road_pred, road_target)
+
+        # CrossEntropyLoss expects (batch, 3) logits and (batch,) labels
+        loss_traffic = self.traffic_loss(traffic_pred, traffic_target)
 
         # CrossEntropyLoss expects (batch, 6) logits and (batch,) labels
+        loss_weather = self.weather_loss(weather_pred, weather_target)
+
+        # CrossEntropyLoss expects (batch, 4) logits and (batch,) labels
         loss_scene = self.scene_loss(scene_pred, scene_target)
 
-        # BCEWithLogitsLoss expects (batch,) logits and (batch,) labels
-        loss_road = self.road_loss(road_pred.squeeze(), road_target.float())
-
-        # BCEWithLogitsLoss expects (batch,) logits and (batch,) labels
-        loss_traffic = self.traffic_loss(traffic_pred.squeeze(), traffic_target.float())
+        # CrossEntropyLoss expects (batch, 4) logits and (batch,) labels
+        loss_night = self.night_loss(night_pred, night_target)
 
         # Total loss (weighted sum if needed)
         total_loss = (
-            loss_night
+            loss_fog
             + loss_glare
-            + loss_weather
-            + loss_scene
             + loss_road
             + loss_traffic
+            + loss_weather
+            + loss_scene
+            + loss_night
         )
         return total_loss
